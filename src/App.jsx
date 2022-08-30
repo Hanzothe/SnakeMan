6b907dd52c4b844b96659c735f90643197f7a4e1
Hanzothe <gui_oliveirasantos123@hotmail.com>
2022-08-30 15:51:09 -0300

    Building a canvas for our Matrix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import Canvas from "./Canvas";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Canvas />
     </div>
   );
 }
