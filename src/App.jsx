a2f8175d7e37e30ff66bd7021643fd35312f281f
Hanzothe <103909243+Hanzothe@users.noreply.github.com>
2022-09-01 23:11:57 -0300

    ;

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [snakeDots, setSnakeDots] = useState([
+    [0, 0],
+    [2, 0],
+  ]);
+
   return (
     <div className="App">
       <div className="playablearea">
